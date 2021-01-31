0f342ac0c20cbc04555ab3f53ee1f6e072232e1e
rosagp75 <gar19109@byu.edu>
2021-01-30 22:29:59 -0500

    initial commit

---
+++ lesson4-wednesday/js/scripts.js
@@ -0,0 +1,14 @@
+function toggleMenu(){
+       document.getElementById("primaryNav").classList.toggle("hide");
+}
+function CalcularFactor() {
+       var t = parseFloat(document.getElementById('dato1').value);
+       var s = parseFloat(document.getElementById('dato2').value);
+       if (t<=50 && s>3){
+              f=35.74+(0.6215*t)-(35.75*Math.pow(s,0.16))+(0.4275*t*Math.pow(s,0.16));
+       }
+       else{
+              f="N/A";
+       }
+       document.getElementById('outputDiv').innerHTML =f;
+}
