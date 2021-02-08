86125137454e2fe579d1b1755787c2412fc8ecb6
rosagp75 <gar19109@byu.edu>
2021-02-07 23:23:46 -0500

    initial commit

---
+++ lesson10-saturday/js/weatherapi.js
@@ -0,0 +1,30 @@
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=58f3ede8a932ca945faf8e7bf4d71898';
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    //pass values to local var
+    var t=parseFloat(jsObject.main.temp);
+    var humidity=parseFloat(jsObject.main.humidity);
+    const desc = jsObject.weather[0].description;
+    var s=parseFloat(jsObject.wind.speed);
+    //calculate chillFactor
+    if (t<=50 && s>3){
+      f=35.74+(0.6215*t)-(35.75*Math.pow(s,0.16))+(0.4275*t*Math.pow(s,0.16));
+    }
+    else{
+      f="N/A";
+    }
+    document.getElementById('txtdescripcion').innerHTML = desc; 
+    document.getElementById('txttemperatura').innerHTML = t;
+    document.getElementById('txthumidity').textContent = humidity;
+    document.getElementById('txtspeed').textContent = s;
+    document.getElementById('txtfactor').textContent = f;
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+      // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
+  
