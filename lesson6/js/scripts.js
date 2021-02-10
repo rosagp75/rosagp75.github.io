a0999840ca9bf5a34014cdd25e9abb402a1657c5
rosagp75 <gar19109@byu.edu>
2021-02-10 17:24:06 -0500

    initial commit

---
+++ lesson6/js/scripts.js
@@ -0,0 +1,15 @@
+function toggleMenu(){
+       document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+function adjustRating(rating) {
+       document.getElementById("ratingvalue").innerHTML = rating;
+   }
+   
+   function selectResponse() {
+          const s = document.querySelector('#selected')
+          const sel = document.querySelector('#selectbrowser');
+          s.style.display = "block";
+          s.textContent = sel.value;
+          
+   }
