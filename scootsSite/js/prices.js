836a0ea2446549b8e281ce931a86f4d102fe4c48
rosagp75 <gar19109@byu.edu>
2021-02-20 18:41:12 -0500

    initial commit

--- scootsSite/js/prices.js
+++ scootsSite/js/prices.js
@@ -17,39 +17,39 @@ fetch(requestURL)
         }
         if (i==1){
           document.getElementById('max1').innerHTML = renta[i].max; 
-          document.getElementById('pre1-1').innerHTML = renta[i].Reservation-Half-Day; 
-          document.getElementById('pre1-2').innerHTML = renta[i].Reservation-Full-Day; 
-          document.getElementById('pre1-3').innerHTML = renta[i].Walk-Half-Day; 
-          document.getElementById('pre1-4').innerHTML = renta[i].Walk-Full-Day;
+          document.getElementById('pre1-1').innerHTML = renta[i].ReservationHalfDay; 
+          document.getElementById('pre1-2').innerHTML = renta[i].ReservationFullDay; 
+          document.getElementById('pre1-3').innerHTML = renta[i].WalkHalfDay; 
+          document.getElementById('pre1-4').innerHTML = renta[i].WalkFullDay;
           }
 
         if (i==2){
             document.getElementById('max2').innerHTML = renta[i].max; 
-            document.getElementById('pre2-1').innerHTML = renta[i].Reservation-Half-Day; 
-            document.getElementById('pre2-2').innerHTML = renta[i].Reservation-Full-Day; 
-            document.getElementById('pre2-3').innerHTML = renta[i].Walk-Half-Day; 
-            document.getElementById('pre2-4').innerHTML = renta[i].Walk-Full-Day;
+            document.getElementById('pre2-1').innerHTML = renta[i].ReservationHalfDay; 
+            document.getElementById('pre2-2').innerHTML = renta[i].ReservationFullDay; 
+            document.getElementById('pre2-3').innerHTML = renta[i].WalkHalfDay; 
+            document.getElementById('pre2-4').innerHTML = renta[i].WalkFullDay;
             }
         if (i==3){
             document.getElementById('max3').innerHTML = renta[i].max; 
-            document.getElementById('pre3-1').innerHTML = renta[i].Reservation-Half-Day; 
-            document.getElementById('pre3-2').innerHTML = renta[i].Reservation-Full-Day; 
-            document.getElementById('pre3-3').innerHTML = renta[i].Walk-Half-Day; 
-            document.getElementById('pre3-4').innerHTML = renta[i].Walk-Full-Day;
+            document.getElementById('pre3-1').innerHTML = renta[i].ReservationHalfDay; 
+            document.getElementById('pre3-2').innerHTML = renta[i].ReservationFullDay; 
+            document.getElementById('pre3-3').innerHTML = renta[i].WalkHalfDay; 
+            document.getElementById('pre3-4').innerHTML = renta[i].WalkFullDay;
             }
         if (i==4){
               document.getElementById('max4').innerHTML = renta[i].max; 
-              document.getElementById('pre4-1').innerHTML = renta[i].Reservation-Half-Day; 
-              document.getElementById('pre4-2').innerHTML = renta[i].Reservation-Full-Day; 
-              document.getElementById('pre4-3').innerHTML = renta[i].Walk-Half-Day; 
-              document.getElementById('pre4-4').innerHTML = renta[i].Walk-Full-Day;
+              document.getElementById('pre4-1').innerHTML = renta[i].ReservationHalfDay; 
+              document.getElementById('pre4-2').innerHTML = renta[i].ReservationFullDay; 
+              document.getElementById('pre4-3').innerHTML = renta[i].WalkHalfDay; 
+              document.getElementById('pre4-4').innerHTML = renta[i].WalkFullDay;
             }
         if (i==5){
               document.getElementById('max5').innerHTML = renta[i].max; 
-              document.getElementById('pre5-1').innerHTML = renta[i].Reservation-Half-Day; 
-              document.getElementById('pre5-2').innerHTML = renta[i].Reservation-Full-Day; 
-              document.getElementById('pre5-3').innerHTML = renta[i].Walk-Half-Day; 
-              document.getElementById('pre5-4').innerHTML = renta[i].Walk-Full-Day;
+              document.getElementById('pre5-1').innerHTML = renta[i].ReservationHalfDay; 
+              document.getElementById('pre5-2').innerHTML = renta[i].ReservationFullDay; 
+              document.getElementById('pre5-3').innerHTML = renta[i].WalkHalfDay; 
+              document.getElementById('pre5-4').innerHTML = renta[i].WalkFullDay;
             }
       }
 });
